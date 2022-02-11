d89ca54dcaffcb81751295a5cff929098d32b477
Naman-Saxena1 <namansaxena.official@gmail.com>
2022-02-11 18:07:31 +0530

    feat : Connected Home Page to Category Page

---
+++ index.js
@@ -0,0 +1,5 @@
+const startBtn = document.querySelector("#homepage-start-btn")
+
+startBtn.addEventListener('click',()=>{
+    window.location="./Pages/Quizes-by-Categories/quiz-categories.html"
+})
