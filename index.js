687ce8e0c53a758765092c45c840fe8cd76b7559
Naman-Saxena1 <namansaxena.official@gmail.com>
2022-02-12 13:17:50 +0530

    feat : Updated HomePage and added pages of quizzes according to their categories

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const startBtn = document.querySelector("#homepage-start-btn")
+const viewThrillerQuizzesBtn = document.querySelector("#view-thiller-btn")
+const viewComedyQuizzesBtn = document.querySelector("#view-comedy-btn")
+const viewRomanceQuizzesBtn = document.querySelector("#view-romance-btn")
+const viewDystopiaQuizzesBtn = document.querySelector("#view-dystopia-btn")
+const viewCrimeQuizzesBtn = document.querySelector("#view-crime-btn")
+const viewSuperheroQuizzesBtn = document.querySelector("#view-superhero-btn")
+const viewMedievalFantasyQuizzesBtn = document.querySelector("#view-medieval-fantasy-btn")
+const viewAnimeQuizzesBtn = document.querySelector("#view-anime-btn")
 
-startBtn.addEventListener('click',()=>{
-    window.location="./Pages/Quizes-by-Categories/quiz-categories.html"
+viewThrillerQuizzesBtn.addEventListener('click',()=>{
+    window.location="./Pages/Genre-Thriller-Quizzes/thriller-quizzes.html"
+})
+
+viewComedyQuizzesBtn.addEventListener('click',()=>{
+    window.location="./Pages/Genre-Comedy-Quizzes/comedy-quizzes.html"
+})
+
+viewRomanceQuizzesBtn.addEventListener('click',()=>{
+    window.location="./Pages/Genre-Romance-Quizzes/romance-quizzes.html"
 })
